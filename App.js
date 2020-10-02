f4a567c1daf8425f976181735e0721531f200a3e
patiparn wannago <patiparn.wannago@gmail.com>
2020-10-02 13:11:28 +0700

    commit 08

--- App.js
+++ App.js
@@ -1,17 +1,18 @@
 import React from 'react';
+import React from 'react';
 import { Router, Scene } from 'react-native-router-flux';
 import Home from './Home';
 import Login from './Login';
 
+const Stack = createStackNavigator();
 export default function App() {
   return (        
-      <Router>
+    <Router>
         <Scene key="root">
           <Scene key="Login" component={Login} hideNavBar={true}></Scene>
           <Scene key="Home" component={Home} hideNavBar={true}></Scene>
         </Scene>
-      </Router>
+      </Router>    
 
-     
   );
 }
